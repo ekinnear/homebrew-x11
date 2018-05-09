517be92e0f714c7d3f24a110ae1c85b753ae4e41
ekinnear <ekinnear@users.noreply.github.com>
2018-05-08 20:57:48 -0700

    Update giblib.rb

--- giblib.rb
+++ giblib.rb
@@ -6,7 +6,6 @@ class Giblib < Formula
 
   bottle do
     cellar :any
-    revision 1
     sha256 "e3e8ff19bb06a1b105e351e8c71f05a3ce92c57cc9f8f82f81d0a2f19b0f6edf" => :yosemite
     sha256 "c4622a6c22f066411a69e824f3e3fbfa6d7713d44a5f05ada8058949745ceeb4" => :mavericks
     sha256 "d326b3b552493bc46db47aeafca759afdf4e94db683445be4b3af6656fbd12b3" => :mountain_lion
